2d1233c0ea1f5994da3bb0d713856f4fd0478f12
Adan Saavedra <adansaavedraa@icloud.com>
2023-10-23 20:42:25 -0500

    fix my order route

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,7 +1,26 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
 import Layout from "../../Components/Layout";
+import OrderCard from "../../Components/OrderCard";
 
 function MyOrder() {
-  return <Layout>MyOrder</Layout>;
+  const context = useContext(ShoppingCartContext);
+  return (
+    <Layout>
+      My Order
+      <div className="flex flex-col w-80">
+        {context.order?.slice(-1)[0].products.map((product) => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            imageURL={product.images}
+            price={product.price}
+          />
+        ))}
+      </div>
+    </Layout>
+  );
 }
 
 export default MyOrder;
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,24 +1,9 @@
-import { useContext } from "react";
-import { ShoppingCartContext } from "../../Context";
 import Layout from "../../Components/Layout";
-import OrderCard from "../../Components/OrderCard";
 
 function MyOrders() {
-  const context = useContext(ShoppingCartContext);
   return (
     <Layout>
-      My Order
-      <div className="flex flex-col w-80">
-        {context.order?.slice(-1)[0].products.map((product) => (
-          <OrderCard
-            key={product.id}
-            id={product.id}
-            title={product.title}
-            imageURL={product.images}
-            price={product.price}
-          />
-        ))}
-      </div>
+      <h1>My Orders</h1>
     </Layout>
   );
 }
